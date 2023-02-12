9fb7e271597d3dc86b6931fa8c6637b6c0ebef33
rinikim <tytntu@naver.com>
2023-02-12 19:17:49 +0900

    fix : stackOverFlow error 해결

--- src/main/java/com/fastcampus/sns/exception/SnsApplicationException.java
+++ src/main/java/com/fastcampus/sns/exception/SnsApplicationException.java
@@ -10,11 +10,19 @@ public class SnsApplicationException extends RuntimeException {
     private ErrorCode errorCode;
     private String message;
 
+
+    public SnsApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
     @Override
     public String getMessage() {
         if (message == null) {
             return errorCode.getMessage();
+        } else {
+            return String.format("%s. %s", errorCode.getMessage(), message);
         }
-        return String.format("%s. %s", errorCode, getMessage(), message);
+
     }
 }
