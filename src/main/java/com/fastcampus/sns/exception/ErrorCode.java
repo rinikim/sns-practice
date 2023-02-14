f0b78e6e6cc3db1751362f76d276aed6408dc17c
rinikim <tytntu@naver.com>
2023-02-14 10:23:11 +0900

    feat : errorCode 추가 및 RuntimeException 처리

--- src/main/java/com/fastcampus/sns/exception/ErrorCode.java
+++ src/main/java/com/fastcampus/sns/exception/ErrorCode.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
     private HttpStatus httpStatus;
     private String message;
--- src/main/java/com/fastcampus/sns/exception/GlobalControllerAdvice.java
+++ src/main/java/com/fastcampus/sns/exception/GlobalControllerAdvice.java
@@ -1,6 +1,6 @@
 package com.fastcampus.sns.exception;
 
-import com.fastcampus.sns.response.Response;
+import com.fastcampus.sns.controller.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +15,14 @@ public class GlobalControllerAdvice {
         log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
                 .body(Response.error(e.getErrorCode().name()));
+
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> applicationHandler(RuntimeException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
 
     }
 }
