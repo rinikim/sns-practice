65ecc74aa10dc101bcb7e6724a70976b95baa1c6
rinikim <tytntu@naver.com>
2023-02-14 10:20:40 +0900

    feat : jwt 관련 라이브러리 추가 및 jwt 관련 정보 설정

---
+++ src/main/java/com/fastcampus/sns/utils/JwtTokenUtils.java
@@ -0,0 +1,33 @@
+package com.fastcampus.sns.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtTokenUtils {
+
+    public static String generateToken(String userName, String key, long expiredTimeMs) {
+        Claims claims = Jwts.claims();
+        claims.put("userName", userName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                // 토큰 발행 된 시간
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                // 토큰 만기 시간
+                .setExpiration(new Date(System.currentTimeMillis() + expiredTimeMs))
+                // 키값이랑 알고리즘 정보 필요
+                .signWith(getKey(key), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private static Key getKey(String key) {
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
